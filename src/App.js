d93ed6a070ecf1a5b54f9c89ed953894c30ff311
webdesignvista <info@webdesignvista.com>
2023-07-13 15:01:33 +0530

    2nd commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Mynavbar from './layout/Mynavbar';
+
+function App() {
+  	return (
+    	<div className="App">
+			<Mynavbar />
+    	</div>
+  	);
+}
+
+export default App;
