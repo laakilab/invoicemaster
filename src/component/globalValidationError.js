0676e4dabc3764c495e768e7827512d7502a9595
webdesignvista <laakiphukan@gmail.com>
2024-01-09 22:15:33 +0530

    commit error js

---
+++ src/component/globalValidationError.js
@@ -0,0 +1,9 @@
+let globalValidationError = false
+
+export const getGlobalValidationError = () => globalValidationError
+
+export const setGlobalValidationError = (newValue) => {
+    globalValidationError = newValue
+}
+
+
