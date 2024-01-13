43eaeab96b4104b1876d84c5b471e91997ca8f53
webdesignvista <info@webdesignvista.com>
2024-01-13 20:11:01 +0530

    commit

---
+++ src/component/templates/Basic.js
@@ -0,0 +1,12 @@
+export default function Basic (props) {
+    const stateData = props.stateData
+
+    return <>
+        <div id="printableBlock">
+            <div className="row m-0 text-end pt-3 pb-5">
+                <h2>Invoice</h2>
+            </div>
+        </div>
+    </>
+
+}
